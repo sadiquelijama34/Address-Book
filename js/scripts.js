228f9ffc557714647f63f273ba6386f35afb71f0
sadiquealijama34 <sadiquealijama34@gmail.com>
2017-06-15 12:36:41 +0300

    updated html file js file

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,7 @@ Contact.prototype.fullName = function() {
 
 // user interface logic
 $(document).ready(function() {
-  $("#add-address").append('<div class="new-adress>">' +  '<div class="form-group">' + '<label for="new-street">Street</label>' + '<input type="text"class="form-control-new-street">' + '</div>' + '<div class="form-group">' + '<label for="new-city">City</label>' + '<input type="text" class="form-control-new-city">' + '</div>' + '<div class="form-group">' + '<label for="new-county">County</label>' + '<input type="text" class="form-control new-county">' + '</div>' + '</div>');
+  $("#add-address").append('<div class="new-address>">' +  '<div class="form-group">' + '<label for="new-street">Street</label>' + '<input type="text"class="form-control-new-street">' + '</div>' + '<div class="form-group">' + '<label for="new-city">City</label>' + '<input type="text" class="form-control-new-city">' + '</div>' + '<div class="form-group">' + '<label for="new-county">County</label>' + '<input type="text" class="form-control new-county">' + '</div>' + '</div>');
 });
 
 $(document).ready(function() {
