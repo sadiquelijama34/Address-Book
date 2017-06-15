5ffe626d87ee6d0b95dcfb96c82fe8d16d553149
sadiquealijama34 <sadiquealijama34@gmail.com>
2017-06-15 12:53:12 +0300

    added jquerry logic to form submit listener

--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,16 @@ Contact.prototype.fullName = function() {
 $(document).ready(function() {
   $("#add-address").append('<div class="new-address>">' +  '<div class="form-group">' + '<label for="new-street">Street</label>' + '<input type="text"class="form-control-new-street">' + '</div>' + '<div class="form-group">' + '<label for="new-city">City</label>' + '<input type="text" class="form-control-new-city">' + '</div>' + '<div class="form-group">' + '<label for="new-county">County</label>' + '<input type="text" class="form-control new-county">' + '</div>' + '</div>');
 });
+...
+$(".new-address").each(function() {
+  var inputtedStreet = $(this).find("input.new-street").val();
+  var inputtedCity = $(this).find("input.new-city").val();
+  var inputtedCounty = $(this).find("input.new-county").val();
+  var newAddress = new Address(inputtedStreet, inputtedCity, inputtedCounty);
+  newContact.addresses.push(newAddress);
+
+});
+...
 
 $(document).ready(function() {
   $("form#new-contact").submit(function(event) {
